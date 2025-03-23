ff1bf257fc5b495da79afb056b1f8dceedcf4c89
Silencio <77356433+S1lencio@users.noreply.github.com>
2025-03-23 19:14:50 +0100

    Add hover to map command link

--- src/main/java/de/silencio/customplugin/commands/MapCommand.java
+++ src/main/java/de/silencio/customplugin/commands/MapCommand.java
@@ -7,7 +7,7 @@ import net.kyori.adventure.text.minimessage.MiniMessage;
 public final class MapCommand implements SimpleCommand {
     private static final MiniMessage mm = MiniMessage.miniMessage();
 
-    final static Component mapMessage = mm.deserialize("<aqua>The map can be viewed <gold><u><click:open_url:'https://map.devlencio.net'>here</click></u></gold><aqua>.");
+    final static Component mapMessage = mm.deserialize("<aqua>The map can be viewed <gold><u><hover:show_text:'<gold>Click me!'><click:open_url:'https://map.devlencio.net'>here</click></hover></u></gold><aqua>.");
 
     @Override
     public void execute(final Invocation invocation) { invocation.source().sendMessage(mapMessage); }
